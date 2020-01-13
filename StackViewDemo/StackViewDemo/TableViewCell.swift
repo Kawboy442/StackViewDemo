732fed94efe8a684a645bfd8a2978a6abf0c6327
R.ito <kawboy442@gmail.com>
2020-01-13 17:49:04 +0900

    add: make tableviewcell

---
+++ StackViewDemo/StackViewDemo/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  StackViewDemo
+//
+//  Created by 伊藤僚亮 on 2020/01/13.
+//  Copyright © 2020 Ryosuke.Ito. All rights reserved.
+//
+
+import UIKit
+
+final class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+  
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+    
+}
