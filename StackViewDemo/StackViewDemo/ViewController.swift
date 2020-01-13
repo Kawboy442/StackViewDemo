3ab6d370e44abf07e4ecb22cfcecad853eb0d59f
R.ito <kawboy442@gmail.com>
2020-01-13 17:46:51 +0900

    add: make tableview

--- StackViewDemo/StackViewDemo/ViewController.swift
+++ StackViewDemo/StackViewDemo/ViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
